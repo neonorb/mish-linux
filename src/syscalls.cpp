aa56f26cd399637da0f9ab1721e64860b367a0c2
chris13524 <chris@hichris.com>
2016-10-13 17:52:23 -0400

    Added print syscall.

--- src/syscalls.cpp
+++ src/syscalls.cpp
@@ -14,15 +14,30 @@ Value* printlnFunction(List<Value*>* arguments) {
 	return NULL;
 }
 
+Value* printFunction(List<Value*>* arguments) {
+	std::cout << ((StringValue*) arguments->get(0))->value;
+
+	return NULL;
+}
+
 List<Function*> syscalls;
 
 void registerSyscalls() {
+	// println
 	List<ValueType>* printlnParameterTypes = new List<ValueType>();
 	printlnParameterTypes->add(STRING_VALUE);
 	Function* println = new Function("__println"_H, printlnParameterTypes,
 			VOID_VALUE, printlnFunction);
 	syscalls.add(println);
 	mish_syscalls.add(println);
+
+	// print
+	List<ValueType>* printParameterTypes = new List<ValueType>();
+	printParameterTypes->add(STRING_VALUE);
+	Function* print = new Function("__print"_H, printParameterTypes, VOID_VALUE,
+			printFunction);
+	syscalls.add(print);
+	mish_syscalls.add(print);
 }
 
 void unregisterSyscalls() {
