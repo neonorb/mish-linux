8e0d273ac1711ec79bcdcb8fede19ede991e2d3d
Chris Smith <chris@hichris.com>
2016-11-09 13:54:33 -0500

    Updated to confirm with new Mish code.

--- src/syscalls.cpp
+++ src/syscalls.cpp
@@ -38,23 +38,24 @@ List<Function*> syscalls;
 void registerSyscalls() {
 	// println
 	List<ValueType>* printlnParameterTypes = new List<ValueType>();
-	printlnParameterTypes->add(STRING_VALUE);
+	printlnParameterTypes->add(ValueType::STRING);
 	Function* println = new Function("__println"_H, printlnParameterTypes,
-			VOID_VALUE, printlnFunction);
+			ValueType::VOID, printlnFunction);
 	syscalls.add(println);
 	mish_syscalls.add(println);
 
 	// _print
 	List<ValueType>* printParameterTypes = new List<ValueType>();
-	printParameterTypes->add(STRING_VALUE);
-	Function* print = new Function("__print"_H, printParameterTypes, VOID_VALUE,
-			printFunction);
+	printParameterTypes->add(ValueType::STRING);
+	Function* print = new Function("__print"_H, printParameterTypes,
+			ValueType::VOID, printFunction);
 	syscalls.add(print);
 	mish_syscalls.add(print);
 
 	// exit
 	List<ValueType>* exitParameterTypes = new List<ValueType>();
-	Function* exit = new Function("__exit"_H, exitParameterTypes, VOID_VALUE, exitFunction);
+	Function* exit = new Function("__exit"_H, exitParameterTypes,
+			ValueType::VOID, exitFunction);
 	syscalls.add(exit);
 	mish_syscalls.add(exit);
 }
